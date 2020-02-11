f6ec94989e9d1825b6db994e67b0a5bc879047d2
Joe <38415384+joe247@users.noreply.github.com>
2020-02-11 22:39:31 +0530

    Create 002_fork.py

---
+++ S6/CS334-NPL/002_fork.py
@@ -0,0 +1,13 @@
+# Python code to create child process
+import os
+
+def parent_child():
+    n = os.fork()
+    # n greater than 0 means parent process
+    if n > 0:
+        print("Parent process and id is = ", os.getpid())
+    # n equals to 0 means child process
+    else:
+        print("Child process and id is = ", os.getpid())
+
+parent_child()
