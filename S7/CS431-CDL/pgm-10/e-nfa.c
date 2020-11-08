b2d6e9ced55f69842ffb94c8bcfff8b8d8f34ed0
Jovial Joe Jayarson <jovial7joe@hotmail.com>
2020-11-08 09:18:27 +0530

    fix: fixes seg fault; adds comnts; from given code

--- S7/CS431-CDL/pgm-10/e-nfa.c
+++ S7/CS431-CDL/pgm-10/e-nfa.c
@@ -4,76 +4,72 @@
 #define STATES 256
 #define SYMBOLS 20
 
-int N_SYMBOLS;
-int NFA_STATES, DFA_STATES;
-
-int DFA_TAB[STATES][SYMBOLS];
+int N_SYM, NFA_STATES;
 char *NFA_TAB[STATES][SYMBOLS];
 
-void put_DFA_table(int tab[][SYMBOLS], int n_states, int n_symbols)
+int DFA_STATES; // number of DFA states
+int DFA_TAB[STATES][SYMBOLS];
+
+/*  Print state-transition table.  */
+void put_dfa_table(
+    int tab[][SYMBOLS], // DFA table
+    int nstates,        // number of states
+    int nsymbols)       // number of input symbols
 {
     int i, j;
-    puts("\nState transition table\n");
-    printf(" | ");
-
-    for (i = 0; i < n_symbols; i++)
-        printf("%c", '0' + i);
-    
-    printf("\n+--");
-
-    for (i = 0; i < n_symbols; i++)
+    puts("\nSTATE TRANSITION TABLE");
+    /*  input symbols: '0', '1', ...  */
+    printf("   | ");
+    for (i = 0; i < nsymbols; i++)
+        printf(" %c ", '0' + i);
+    printf("\n---+-------");
+    for (i = 0; i < nsymbols; i++)
         printf(" ");
-    
     printf("\n");
-
-    for (i = 0; i < n_states; i++)
+    for (i = 0; i < nstates; i++)
     {
-        printf("%c |", 'A' + i);
-        for (j = 0; j < n_states; j++)
-        {
-            printf("%c ", 'A' + tab[i][j]);
-            printf("\n");
-        }
+        printf(" %c | ", 'A' + i); // state
+        for (j = 0; j < nsymbols; j++)
+            printf(" %c ", 'A' + tab[i][j]);
+        printf("\n");
     }
 }
 
+/*  Initialize NFA table.  */
 void init_NFA_table()
 {
     /*
-    const char *nfa_tab[STATES][SYMBOLS] = {{"12", "13"}, {"18", "13"}, {"4", "17"}, {"18", "4"}, {"4", "4"}};
-
-    int i, j;
-
-    for (i = 0; i < 5; i++)
-    {
-        for (j = 0; j < 2; j++)
-        {
-            NFA_TAB[i][j] = nfa_tab[i][j];
-        }
-    }
+        NFA table for ex.21 at p.76 NFA_TAB[0][0] = "01";
+        NFA_TAB[0][1] = "0";
+        NFA_TAB[1][0] = "";
+        NFA_TAB[1][1] = "01";
+        NFA_STATES = 2;
+        DFA_STATES = 0; N_SYM = 2;
     */
+
+    // NFA table for ex.17 at p.72
     NFA_TAB[0][0] = "12";
     NFA_TAB[0][1] = "13";
     NFA_TAB[1][0] = "12";
-    NFA_TAB[0][1] = "13";
+    NFA_TAB[1][1] = "13";
     NFA_TAB[2][0] = "4";
     NFA_TAB[2][1] = "";
     NFA_TAB[3][0] = "";
     NFA_TAB[3][1] = "4";
     NFA_TAB[4][0] = "4";
     NFA_TAB[4][1] = "4";
-
     NFA_STATES = 5;
     DFA_STATES = 0;
-    N_SYMBOLS = 2; // 0 & 1
+    N_SYM = 2;
 }
 
+/*  String 't' is merged into 's' in an alphabetical order.  */
 void string_merge(char *s, char *t)
 {
     char temp[STATES], *r = temp, *p = s;
     while (*p && *t)
     {
-        if (*p == *t) // same state transation
+        if (*p == *t)
         {
             *r++ = *p++;
             t++;
@@ -85,61 +81,62 @@ void string_merge(char *s, char *t)
         else
             *r++ = *t++;
     }
-
-    *r++ = '\0';
-
+    *r = '\0';
     if (*p)
         strcat(r, p);
     else if (*t)
         strcat(r, t);
-
     strcpy(s, temp);
 }
 
-void get_next_state(char *next_states, char *curr_states, char *nfa[STATES][SYMBOLS], int n_nfa, int symbols)
+/*  Get next-state string for current-state string.  */
+void get_next_state(char *nextstates, char *cur_states, char *nfa[STATES][SYMBOLS], int n_nfa, int symbol)
 {
     int i;
     char temp[STATES];
     temp[0] = '\0';
-    for (i = 0; i < strlen(curr_states); i++)
-        string_merge(temp, nfa[curr_states[i] - '0'][symbols]);
-    strcpy(next_states, temp);
+    for (i = 0; i < strlen(cur_states); i++)
+        string_merge(temp, nfa[cur_states[i] - '0'][symbol]);
+    strcpy(nextstates, temp);
 }
-
-int state_index(char *state, char state_name[][STATES], int *pn)
+int state_index(char *state, char statename[][STATES], int *pn)
 {
     int i;
     if (!*state)
-        return -1;
+        return -1; // no next state
     for (i = 0; i < *pn; i++)
-    {
-        if (!strcmp(state, state_name[i]))
+        if (!strcmp(state, statename[i]))
             return i;
-        strcpy(state_name[i], state);
-    }
-    return *pn++;
+    strcpy(statename[i], state);
+    return (*pn)++; // new state-name 
 }
 
+/*  Convert NFA table to DFA table. Return value: number of DFA states.  */
 int nfa_to_dfa(char *nfa[STATES][SYMBOLS], int n_nfa, int n_sym, int dfa[][SYMBOLS])
 {
-    int i = 0, j, n = 1;
-    char state_name[STATES][STATES], next_state[STATES];
-    strcpy(state_name[0], "0");
+    char statename[STATES][STATES];
+    int i = 0; // current index of DFA
+    int n = 1; // number of DFA states
+    char nextstate[STATES];
+    int j;
+    strcpy(statename[0], "0"); // start state
+    // for each DFA state
     for (i = 0; i < n; i++)
     {
+        // for each input symbol
         for (j = 0; j < n_sym; j++)
         {
-            get_next_state(next_state, state_name[i], nfa, n_nfa, j);
-            dfa[i][i] = state_index(next_state, state_name, &n);
+            get_next_state(nextstate, statename[i], nfa, n_nfa, j);
+            dfa[i][j] = state_index(nextstate, statename, &n);
         }
     }
-    return n;
+    return n; // number of DFA states
 }
 
 int main()
 {
     init_NFA_table();
-    DFA_STATES = nfa_to_dfa(NFA_TAB, NFA_STATES, N_SYMBOLS, DFA_TAB);
-    put_DFA_table(DFA_TAB, DFA_STATES, N_SYMBOLS);
+    DFA_STATES = nfa_to_dfa(NFA_TAB, NFA_STATES, N_SYM, DFA_TAB);
+    put_dfa_table(DFA_TAB, DFA_STATES, N_SYM);
     return 0;
 }
